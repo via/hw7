cd3fe581d68f0ae9245a207abba2fdbd58f72b7b
Kevin <pacifist@pacifist-laptop.(none)>
2011-05-02 22:30:46 -0400

    Added ConnectDialog Class and ui. I also created an instance and displayed it in mainwindow's constructor. (This code seg faults)

---
+++ phoneClient/connectdialog.cpp
@@ -0,0 +1,20 @@
+#include "connectdialog.h"
+#include "ui_connectdialog.h"
+
+ConnectDialog::ConnectDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ConnectDialog)
+{
+    ui->setupUi(this);
+}
+
+ConnectDialog::~ConnectDialog()
+{
+    delete ui;
+}
+
+void ConnectDialog::on_connectButton_clicked()
+{
+    //connection magic
+       //emit signal to thread class slot
+}
